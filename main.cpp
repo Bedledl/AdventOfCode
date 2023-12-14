cb071e350e941840eff4707de2335a338f4c1b38
Bettina Riedl <riedl.bettina@freenet.de>
2023-12-14 14:25:51 +0100

    add INPUT_FILES_DIR constant

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 #include "Day4/include/puzzle4.h"
 
+constexpr std::string input_files_dir()  { return INPUT_FILES_DIR; };
+
 int main(void) {
     std::cout << "These are my solutions for the Advent of Code 2023 games!\n";
 }
