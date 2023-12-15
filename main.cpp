b8c4c6b6eb96667585788933532ebf81a9576aa7
Bettina Riedl <riedl.bettina@freenet.de>
2023-12-15 10:45:49 +0100

    add calls to puzzles in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 
+#include "Day1/include/puzzle1.h"
+#include "Day2/include/puzzle2.h"
 #include "Day4/include/puzzle4.h"
 
 constexpr std::string input_files_dir()  { return INPUT_FILES_DIR; };
 
+std::string input_file(std::string puzzle_name) {
+    return input_files_dir() + "input" + puzzle_name + ".txt";
+}
+
+bool input_file_avail(std::string puzzle_name) {
+    return std::filesystem::exists(input_file(puzzle_name));
+}
+
+void init_input(Input &input) {
+    if (input_file_avail(input.puzzle_name)){
+        input.init_from_file(input_file(input.puzzle_name));
+    }
+    else {
+        input.init_from_stream(std::cin);
+    }
+}
+
 int main(void) {
     std::cout << "These are my solutions for the Advent of Code 2023 games!\n";
+
+    // This is currently very un-beautiful
+    day1::Input1 input1("1");
+    init_input(input1);
+    run_puzzle_1(input1);
+
+    Input2 input2 = Input2("2");
+    init_input(input2);
+    run_puzzle_2(input2);
+
+    Input4 input4 = Input4("4");
+    init_input(input4);
+    run_puzzle_4(input4);
+
 }
