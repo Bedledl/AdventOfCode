ab791544c11088116971f8cbf0337e4274e5f7ba
Bettina Riedl <riedl.bettina@freenet.de>
2023-12-18 10:42:15 +0100

    add documentation to Input and Output

--- utils/include/io.h
+++ utils/include/io.h
@@ -5,17 +5,32 @@
 #include <string>
 #include <vector>
 
+
+
+/// @brief Base class for puzzle inputs storing the input data
+///
+/// This is the base class for puzzle inputs. For each puzzle create an individual subclass
+/// that can then contain specialised attributes containing the input data.
+///
 class Input {
     public:
+    /// @brief Name of the Puzzle for which this input object is created. 
     std::string puzzle_name;
     Input() = delete;
     Input(std::string puzzle_name) : puzzle_name(puzzle_name) {};
     virtual ~Input() = default;
 
+    /// @brief Fills this input with data from the file ´path´
+    /// @param path Path to the file containing data
     void init_from_file(std::filesystem::path path);
+
+    /// @brief Fills this input with data from stream
+    /// @param input An input stream e.g. std::cin
     void init_from_stream(std::istream &input);
     private:
-    // we can pass lines by value, but better use an rvalue:)
+
+    /// @brief Called with lines containing the input data. Implements how this data is stored.
+    /// @param lines raw input data e.g. lines read from file
     virtual void set_content(std::vector<std::string> lines) = 0;
 
 //    protected:
@@ -24,6 +39,7 @@ class Input {
 //    Input& operator=(const Input&) = delete;
 };
 
+/// @brief Currently not used!
 struct Output {
     Output();
     virtual void to_file();
